f073a5b535c561a47220119a27c37a05fe3d9c93
LRFSousa <luisricardofs@gmail.com>
2023-09-18 20:43:18 -0300

    listFiles and mkdir

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,30 +1,37 @@
 package application;
-/*
+
 import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner (System.in);
 		
-		File file = new File("c:\\windows\\temp\\in.txt");
-		Scanner sc = null;
-		try {
-			sc = new Scanner(file);
-			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
-			}
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS:");
+		for (File folder : folders) {
+			System.out.println(folder);
 		}
-		catch(IOException e) {
-			System.out.println("Error: "+e.getMessage());
+		
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("FILES:");
+		for (File file : files) {
+			System.out.println(file);
 		}
-		finally {
-			if(sc != null) {
-				sc.close();
-			}
-		}
-
+		
+		boolean success = new File(strPath + "\\subDir").mkdir();
+		System.out.println("Directory created successfully: " + success);
+		
+		sc.close();
 	}
+		
+		
 
-}*/
+}
